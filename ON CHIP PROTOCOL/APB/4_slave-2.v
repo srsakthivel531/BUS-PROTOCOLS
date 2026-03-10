8abbb8bb83cd4a53bdbf96de8a73cff58dd412dd
Sakthivel S <srsakthivel531@gmail.com>
2026-03-10 10:52:14 +0530

    Update 4_slave-2.v

--- ON CHIP PROTOCOL/APB/4_slave-2.v
+++ ON CHIP PROTOCOL/APB/4_slave-2.v
@@ -1,26 +1,19 @@
-//UART SLAVE2
-module UART #(parameter ADDR=16,DATA=8)(input pwrite,penable,pclk,preset_n,input [1:0]psel,input [ADDR-1:0]paddr,input [DATA-1:0]pwdata,output reg [DATA-1:0]prdata,output reg pready);
+module UART #(parameter ADDR=16,DATA=8)(input pwrite,penable,pclk,preset_n,input [1:0]psel,input[ADDR-1:0]paddr,input [DATA-1:0]pwdata,output reg [DATA-1:0]prdata,output reg pready);
 reg [DATA-1:0]mem[0:15];
 always@(posedge pclk or negedge preset_n)
 begin
  if(!preset_n)
-    pready<=0;
-else if(psel==2'b01 && penable)
-    pready<=1;
-else
-    pready<=0;
-end
-always@(posedge pclk)
-begin
- if(psel==2'b01)
- begin
-   if(penable)
+    prdata<=0;
+ else begin
+   pready<=0;
+   if(psel==2'b10 && penable)
    begin
+     pready<=1;
      if(pwrite)
         mem[paddr[3:0]]<=pwdata;
      else
         prdata<=mem[paddr[3:0]];
    end
  end
-end
-endmodule
+end 
+ endmodule
