f68eb7023d198806fdc06212a2217dd56f02bfd1
Sakthivel S <srsakthivel531@gmail.com>
2026-03-10 10:51:49 +0530

    Update 3_slave-1.v

--- ON CHIP PROTOCOL/APB/3_slave-1.v
+++ ON CHIP PROTOCOL/APB/3_slave-1.v
@@ -1,27 +1,21 @@
 
 //TIMER slave1
-module TIMER #(parameter ADDR=16,DATA=8)(input pwrite,penable,pclk,preset_n,input [1:0]psel,input [ADDR-1:0]paddr,input [DATA-1:0]pwdata,output reg [DATA-1:0]prdata,output reg pready);
+module TIMER #(parameter ADDR=16,DATA=8)(input pwrite,penable,pclk,preset_n,input [1:0]psel,input [ADDR-1:0]paddr,input [DATA-1:0]pwdata,output reg [DATA-1:0]prdata,output reg  pready);
 reg [DATA-1:0]mem[0:15];
 always@(posedge pclk or negedge preset_n)
 begin
  if(!preset_n)
-    pready<=0;
- else if(psel==2'b00 && penable)
-    pready<=1;
-  else
-    pready<=0;
-end
-always@(posedge pclk)
-begin
- if(psel==2'b00)
- begin
-   if(penable)
+   prdata<=0;
+ else begin
+   pready<=0;
+   if(psel==2'b1 && penable)
    begin
+     pready<=1;
      if(pwrite)
         mem[paddr[3:0]]<=pwdata;
      else
         prdata<=mem[paddr[3:0]];
+   end 
    end
  end
-end
 endmodule
